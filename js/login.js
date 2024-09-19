58d89250c70b935599e0fd313b05ba9bd624b7af
Abubokkor98 <mail.abubokkor@gmail.com>
2024-09-19 11:12:22 +0600

    Balace display and user action button added

--- js/login.js
+++ js/login.js
@@ -1,11 +1,18 @@
 // step - 1: set event handler
-document.getElementById('btn-login').addEventListener('click', function(event)
-{
+document.getElementById('btn-login').addEventListener('click', function(event){
     // step - 2: prevent default behavior (reloading browser)
     event.preventDefault();
-    console.log('login button clicked') 
-
-    // step - 3: get the phone number
+    // step - 3: get the phone number and the pin
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
-})
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber);
+
+    // bad way to validate, [i will learn good and perfect way in couple of days]
+    if(phoneNumber === '5' && pinNumber ==='1234'){
+        console.log('you are logged in')
+        window.location.href = '/home.html'
+    }
+    else{
+        alert('wrong phone number or pin')
+    }
+});
